807af28f50e8a96e0b8b874ea12ab4fc70c696be
Hisban Shiraz <hisbanbardai@gmail.com>
2023-11-13 16:19:44 -0700

    Implemented parameterized query to protect from sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,20 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
+const queryString = `
 SELECT students.id as id, students.name as name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-LIMIT ${process.argv[3]};
-`
-  )
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then((res) => {
     console.log(res.rows);
   })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,23 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-ORDER BY teacher;
-`
-  )
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+`;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+
+pool
+  .query(queryString,values)
   .then((res) => {
     for (const row of res.rows) {
       console.log(`${row.cohort}: ${row.teacher}`);
